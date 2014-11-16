9337e6ad90524510b6d7669e21403581aa73c3c6
Matthew Wang <mmwang@stanford.edu>
2014-11-16 02:30:00 -0800

    detection data done collecting

--- faceURLs/detect.sh
+++ faceURLs/detect.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-for var in 2 3 4 5 6 7 8
+for var in 1 2 3 4 5 6 7 8
 do
-	./detect_and_id $var > $var_detected.txt
+	echo "PROCESSING FILE $var"
+	./detect_and_id $var > ${var}_detected.txt
 done
--- faceURLs/detect_and_id.cc
+++ faceURLs/detect_and_id.cc
@@ -11,7 +11,6 @@ int main(int argc, char *argv[]) {
 	string line;
 	while(getline(URLDataStream, line)) {
 		string URL = line.substr(3);
-		cout << line[0] << line[1] << endl;
 		string cmd = "curl \"http://apius.faceplusplus.com/v2/detection/detect?api_key=2b330a1611e4780dee14b2666c2e6ef7&api_secret=TOMk3pSLwtrBut3S7VDER6PxrxauYABl&url=" + URL + "%3Fv%3D2&attribute=smiling%2Cpose&tag=" + line.substr(0, 2) + "\"";
 		system(cmd.c_str());
 	}
