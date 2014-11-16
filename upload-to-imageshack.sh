da3ce8c83e069cea86ec865a3449358fe60016d6
Matthew Wang <mmwang@stanford.edu>
2014-11-15 23:03:37 -0800

    raw-data.txt contains output

---
+++ upload-to-imageshack.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+echo $1
+echo $1 >> raw-data.txt
+curl -H Expect: -F key="06GIJRTW05a186fcad387195ed87c5d3ab0e2395" -F fileupload="@$1" -F xml=yes -# "https://api.imageshack.com/v2/images" >> raw-data.txt
