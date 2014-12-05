3eba821e0a910deaeabc50769eb574adfd7527c3
Spencer Yee <spencery@stanford.edu>
2014-12-04 22:10:10 -0800

    deleted jaffe copy

--- JAFFEURL copy/detect.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-for var in 1 2 3 4 5 6 7 8
-do
-    echo "PROCESSING FILE $var"
-    ./detect_and_id ${var}.txt > ${var}_detected.txt
-done
--- JAFFEURL copy/parse_landmarks.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-for var in 1 2 3 4 5 6 7 8
-do
-    echo "PROCESSING FILE $var"
-    ./parse_landmarks ${var}_landmarked.txt
-done
